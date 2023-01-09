48e5b7630db79e7b2097f9323b704d58fd6f798c
Archanaanuss <113876122+Archanaanuss@users.noreply.github.com>
2023-01-09 10:12:29 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import React from 'react';
+import Todo from './Components/Todoapp/Todo';
+
+function App() {
+  return (
+    <div className="App">
+      <Todo/>
+  
+    </div>
+  );
+}
+
+export default App;
